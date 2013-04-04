c34a1b6252b87663a473e0e1f9120c9f49304cfa
sumith <sumith@mobme.in>
2013-04-04 23:21:24 +0530

    A simple twitter search application

---
+++ egghead/twitter_search/twitter.js
@@ -0,0 +1,11 @@
+var twitter = angular.module('twitter', ["ngResource"]);
+
+function TwitterCtrl($scope, $resource){
+	$scope.twitter = $resource("http://search.twitter.com/:action",
+	{action:"search.json", q:'angularjs', callback:'JSON_CALLBACK'},
+	{get: {method: "JSONP"}});
+
+	$scope.doSearch = function(){
+		$scope.twitterResult = $scope.twitter.get({q: $scope.searchTerm});
+	}
+}
